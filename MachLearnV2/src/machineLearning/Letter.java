74fb7f9bd6fb4ab684bd4ce2a5f50cfb42ddd6be
BaileyTibbetts <BaileyTibbetts@baileys-mbp-573.home>
2020-05-30 10:54:19 -0700

    Commit #1: Letter class

--- MachLearnV2/src/machineLearning/Letter.java
+++ MachLearnV2/src/machineLearning/Letter.java
@@ -1,5 +1,33 @@
 package machineLearning;
 
 public class Letter {
- //bruh this do be another change
+	//CONSTANTS
+	int PIXEL_LENGTH =24;
+	//MEMBER VARIABLES
+	float vertical;
+	float horizontal;
+	float rightDiagonal;
+	float leftDiagonal;
+	ImageChar ic = new ImageChar();
+	//CONSTRUCTOR
+	public Letter(boolean isUnknown) {
+		if(!isUnknown)fillRNG();
+		else 
+		{
+			getLines();
+		}
+	}
+	
+	//MEMBER METHODS
+	
+	public void fillRNG() {
+		 vertical = (float) Math.random();
+		 horizontal = (float) Math.random();
+		 rightDiagonal = (float) Math.random();
+		 leftDiagonal = (float) Math.random();
+	}//end of RNG thing
+	
+	public void getLines() {
+		
+	}
 }
